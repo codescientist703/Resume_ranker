9bf70796e9fd3c39e8d045e8d31ab277e03f9820
codescientist703 <pukimonlegend@gmail.com>
2020-02-07 17:13:53 +0530

    Added login and logout functionality

--- app/flasksite/form.py
+++ app/flasksite/form.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flasksite.models  import User
 
 class RegistrationForm(FlaskForm):
 	username = StringField('Username', 
@@ -11,6 +12,20 @@ class RegistrationForm(FlaskForm):
 	confirm_password = PasswordField('Confirm Password', 
 									  validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Sign Up')
+
+	def validate_username(self, username):
+
+		user = User.query.filter_by(username=username.data).first()
+
+		if user:
+			raise ValidationError('That username is already registered. Please choose different one.')
+
+	def validate_email(self, email):
+
+		user = User.query.filter_by(email=email.data).first()
+
+		if user:
+			raise ValidationError('That email is already registered. Please choose different one.')
 
 class LoginForm(FlaskForm):
 	email = StringField('Email', 
--- app/flasksite/routes.py
+++ app/flasksite/routes.py
@@ -1,7 +1,8 @@
 from flasksite.models import User, Post
 from flasksite import app, db, bcrypt
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from flasksite.form import RegistrationForm, LoginForm
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
 	{
@@ -22,6 +23,9 @@ def home():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
+
 	form = RegistrationForm()
 	if form.validate_on_submit():
 		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -29,17 +33,34 @@ def register():
 		db.session.add(user)
 		db.session.commit()
 		flash(f'Your account has been created! You can now login !', 'success')
-		return redirect(url_for('home'))
+		return redirect(url_for('login'))
 	return render_template('register.html',title='Register', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
+
 	form = LoginForm()
 	if form.validate_on_submit():
-		if form.email.data == 'nihalgreat@site.com' and form.password.data == '1234':
-			flash('You are logged in!','success')
-			return redirect(url_for('home'))
+		user = User.query.filter_by(email=form.email.data).first()
+		if user and bcrypt.check_password_hash(user.password, form.password.data):
+			login_user(user, remember=form.remember.data)
+			next_page = request.args.get('next')
+			return redirect(next_page) if next_page else redirect(url_for('home'))
 		else:
 			flash('Login Unsuccessful. Please check email and password.','danger')
 
 	return render_template('login.html',title='login', form=form)
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required
+def account():
+	return render_template('account.html',title='Account')
+
+
