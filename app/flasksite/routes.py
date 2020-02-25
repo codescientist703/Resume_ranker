8693bc0fe3d954766d8566efafa2178ce527a811
codescientist703 <pukimonlegend@gmail.com>
2020-02-25 18:09:14 +0530

    Added requirement.txt

--- app/flasksite/routes.py
+++ app/flasksite/routes.py
@@ -5,13 +5,31 @@ from flasksite.form import RegistrationForm, LoginForm, UpdateAccountForm
 from flask_login import login_user, current_user, logout_user, login_required
 from flasksite.main import execute
 import pandas as pd
-posts = [
-	{
-		'author' : 'Nihal Mittal',
-		'title' : 'Post 1'
-	}
+import random, copy
 
-]
+original_questions = {
+ 'Taj Mahal':['Agra','New Delhi','Mumbai','Chennai'],
+ 'Great Wall of China':['China','Beijing','Shanghai','Tianjin'],
+ 'Petra':['Ma\'an Governorate','Amman','Zarqa','Jerash'],
+ 'Machu Picchu':['Cuzco Region','Lima','Piura','Tacna'],
+ 'Egypt Pyramids':['Giza','Suez','Luxor','Tanta'],
+ 'Colosseum':['Rome','Milan','Bari','Bologna'],
+ 'Christ the Redeemer':['Rio de Janeiro','Natal','Olinda','Betim']
+}
+questions = copy.deepcopy(original_questions)
+
+
+def shuffle(q):
+ selected_keys = []
+ i = 0
+ while i < len(q):
+  current_selection = random.choice(list(q.keys()))
+  if current_selection not in selected_keys:
+   selected_keys.append(current_selection)
+   i = i+1
+ return selected_keys
+
+
 @app.route('/')
 @app.route('/about')
 def about():
@@ -30,13 +48,23 @@ def home():
 
 	return render_template("home.html")
 
-@app.route('/generate')
-def generate():
-	main_frame = pd.DataFrame()
-	print(job_profile)
-	main_frame = execute(job_profile)
-	
-	return render_template('views.html', tables=[main_frame.to_html(index=False)])
+@app.route('/quiz')
+def quiz():
+	questions_shuffled = shuffle(questions)
+	for i in questions.keys():
+		random.shuffle(questions[i])
+
+	return render_template('quiz.html', q = questions_shuffled, o = questions)
+
+@app.route('/quizans', methods=['POST'])
+def quiz_answers():
+	correct = 0
+	for i in questions.keys():
+		answered = request.form[i]
+		if original_questions[i][0] == answered:
+			correct = correct+1
+
+	return '<h1>Correct Answers: <u>'+str(correct)+'</u></h1>'
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
